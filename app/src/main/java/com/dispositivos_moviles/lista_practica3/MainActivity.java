7963baad7e6e2ff348a9b733ee202573ace6a81a
Samuel <samuelpampillonroa@gmail.com>
2023-10-28 16:55:17 +0200

    Ejercicio 3

--- app/src/main/java/com/dispositivos_moviles/lista_practica3/MainActivity.java
+++ app/src/main/java/com/dispositivos_moviles/lista_practica3/MainActivity.java
@@ -3,250 +3,128 @@ package com.dispositivos_moviles.lista_practica3;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.DatePickerDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ArrayList<Actividad> actividades; //Lista con las Actividades
-    private ArrayList<String> nombres; //Lista con los nombres de las actividades para mostrarlas en el ListView
-    private ArrayAdapter<String> adapter; //Adaptador para mostrar la lista en el ListView
+    private ArrayList<Entrenamiento> entrenos;
+    private ArrayAdapter<Entrenamiento> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView lvActividades = (ListView) findViewById(R.id.lvCompra);
-        Button bAdd = (Button) findViewById(R.id.button2);
+        ListView lvEntrenamientos = (ListView) findViewById(R.id.lv_tablaEntrenos);
+        Button btAdd = (Button) findViewById(R.id.bAdd);
+        Button btStats = (Button) findViewById(R.id.bStats);
 
-        //Habilitamos un listener del boton para poder añadir una Actividad
-        bAdd.setOnClickListener(new View.OnClickListener() {
+        //Iniciamos las listas
+        entrenos = new ArrayList<Entrenamiento>();
+        adapter = new ArrayAdapter<Entrenamiento>(this, android.R.layout.simple_list_item_1, entrenos);
+        lvEntrenamientos.setAdapter(adapter);
+
+        //Configuramos el boton de agragar entrenamiento
+        btAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                addActividad();
+                agregarEntrenamiento();
             }
         });
 
-        //Mostramos el ListView con las actividades
-        actividades = new ArrayList<Actividad>();
-        nombres = new ArrayList<String>();
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, nombres);
-
-        lvActividades.setAdapter(adapter);
-
-        //Habilitamos la pulsación larga para editar las Actividades creadas
-        lvActividades.setLongClickable(true);
-        lvActividades.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+        //Configuramos boton de mostrar estadisticas
+        btStats.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                if(position >= 0) {
-                    //Mostramos las opciones
-                    mostrarOpciones(position);
-                }
-
-                return false;
+            public void onClick(View v) {
+                mostrarStats();
             }
         });
-
 
     }
 
-    //Metoodo para añadir una nueva Actividad
-    private void addActividad() {
-        final EditText edText = new EditText(this); // Para que el usuario introduzca el nombre de la nueva actividad
-        final Calendar calendar = Calendar.getInstance(); // Para almacenar la fecha límite
-
-        // Almacenamos la fecha actual para que sea la que se muestre al abrir el DatePickerDialog
-        int currentYear = calendar.get(Calendar.YEAR);
-        int currentMonth = calendar.get(Calendar.MONTH);
-        int currentDay = calendar.get(Calendar.DAY_OF_MONTH);
-
-        // Creamos un AlertDialog para que introduzca el nombre de la nueva Actividad
+    private void agregarEntrenamiento(){
+        //Creamos un AlertDialog
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Nueva Actividad");
-        builder.setMessage("Escriba el nombre de la nueva actividad");
+        builder.setTitle("Añadir Entrenamieno");
+        builder.setMessage("Inroduzca los datos del nuevo entrenamiento");
 
-        // Agregar un EditText para que introduzca el nombre
-        builder.setView(edText);
+        //Inflamos el layout personalizado que creamos para añadir entrenamiento
+        View addView = getLayoutInflater().inflate(R.layout.dialog_agregar_entrenamiento, null);
+        builder.setView(addView);
 
-        //Creamos un DatePickerDialog para seleccionar la fecha límite, lo mostraremos al pulsar el boton de crear
-        DatePickerDialog elegirFecha = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                // Guardamos en el objeto calendar la fecha que ha introducido el usuario
-                calendar.set(year, month, dayOfMonth);
+        EditText etTiempo = addView.findViewById(R.id.etTiempo);
+        EditText etDistancia = addView.findViewById(R.id.etDistancia);
 
-                String nombre = edText.getText().toString();
-                // Nos aseguramos de que haya introducido un nombre
-                if (!nombre.isEmpty()) {
-                    // Creamos la actividad y la añadimos a la lista
-                    Actividad nuevaActividad = new Actividad(nombre, calendar.getTime());
-                    actividades.add(nuevaActividad);
-                    nombres.add(nombre);
-                    adapter.notifyDataSetChanged();
-
-                    // Actualizamos el estado
-                    updateStatus();
-                } else {
-                    Toast.makeText(getApplicationContext(), "Debes escribir un nombre", Toast.LENGTH_LONG).show();
-                }
-            }
-        }, currentYear, currentMonth, currentDay);
-
-
+        //Añadimos botones de crear y cancelar
         builder.setPositiveButton("Crear", new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                //Mostramos el DatePickerDialog
-                elegirFecha.setTitle("Fecha de Caducidad");
-                elegirFecha.setMessage("Selecciona la fecha de caducidad de la Actividad");
-                elegirFecha.show();
+            public void onClick(DialogInterface dialog, int which) {
+                int tiempo = Integer.parseInt(etTiempo.getText().toString()); //Guardamos el tiempo introducido
+                double distancia = Double.parseDouble(etDistancia.getText().toString()); //Guardamos la distancia introducida
+
+                Entrenamiento nuevoEntreno = new Entrenamiento(tiempo, distancia);
+
+                entrenos.add(nuevoEntreno);
+                adapter.notifyDataSetChanged();
             }
         });
 
         builder.setNegativeButton("Cancelar", null);
         builder.create().show();
+
     }
 
-
-    //Metodo para mostrar las opciones de una Actividad seleccionada
-    private void mostrarOpciones(int pos) {
-        final Actividad actividad = actividades.get(pos);
-
+    public void mostrarStats(){
+        //Creamos un AlertDialog
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle(actividad.getNombre());
-        String[] opciones = {"Borrar", "Modificar"};
+        builder.setTitle("Estadisticas Globales");
 
-        builder.setItems(opciones, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int opcion) {
-                if (opcion == 0) { // Borrar
-                    borrarActividad(actividad);
-                } else if (opcion == 1) { // Modificar
-                    editarActividad(actividad, pos);
-                }
-            }
-        });
+        //Inflamos el layout personalizado que creamos para mostrar los datos
+        View statsView = getLayoutInflater().inflate(R.layout.dialog_stats, null);
+        builder.setView(statsView);
 
-        builder.setNegativeButton("Cancelar", null);
+        TextView etTotalKm = (TextView) statsView.findViewById(R.id.tvTotal);
+        TextView etTotalMinKm = (TextView) statsView.findViewById(R.id.tvMinKm);
+
+        //Calculamos los kilometros totales
+        double totalKm = calcularTotalKm();
+        etTotalKm.setText(Double.toString(totalKm));
+
+        //Calculamos la media de minutos por kilometro
+        double mediaMinKm = calcularMediaMinKm();
+        etTotalMinKm.setText(Double.toString(mediaMinKm));
+
+        //Añadimos boton de cerrar
+        builder.setNegativeButton("Cerrar", null);
         builder.create().show();
+
     }
 
-
-    //Codigo para modificar Actividad, similar al de addActividad
-    private void editarActividad(Actividad actividad, int pos){
-        final EditText edText = new EditText(this); // Para que el usuario introduzca el nuevo nombre
-        final Calendar calendar = Calendar.getInstance(); // Para almacenar la nueva fecha límite
-
-        // Almacenamos la fecha del objeto para que sea la que se muestre al abrir el DatePickerDialog
-        calendar.setTime(actividad.getFechaLimite());
-        int oldYear = calendar.get(Calendar.YEAR);
-        int oldMonth = calendar.get(Calendar.MONTH);
-        int oldDay = calendar.get(Calendar.DAY_OF_MONTH);
-
-        //Mostramos el nombre actual
-        edText.setText(actividad.getNombre());
-
-        // Creamos un AlertDialog para que introduzca el nuevo nombre de la Actividad
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle(actividad.getNombre());
-        builder.setMessage("Escriba el nuevo nombre");
-
-        // Agregar un EditText para que introduzca el nombre
-        builder.setView(edText);
-
-        //Creamos un DatePickerDialog para seleccionar la fecha límite, lo mostraremos al pulsar el boton de crear
-        DatePickerDialog elegirFecha = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                // Guardamos en el objeto calendar la fecha que ha introducido el usuario
-                calendar.set(year, month, dayOfMonth);
-
-                String nombre = edText.getText().toString();
-                // Nos aseguramos de que haya introducido un nombre
-                if (!nombre.isEmpty()) {
-                    //Actualizamos la actividad
-                    actividad.setNombre(nombre);
-                    actividad.setFechaLimite(calendar.getTime());
-                    nombres.set(pos, actividad.getNombre());
-                    adapter.notifyDataSetChanged();
-
-                    // Actualizamos el estado
-                    updateStatus();
-                } else {
-                    Toast.makeText(getApplicationContext(), "Debes escribir un nombre", Toast.LENGTH_LONG).show();
-                }
-            }
-        }, oldYear, oldMonth, oldDay);
-
-
-        builder.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                //Mostramos el DatePickerDialog
-                elegirFecha.setTitle("Fecha de Caducidad");
-                elegirFecha.setMessage("Selecciona la fecha de caducidad de la Actividad");
-                elegirFecha.show();
-            }
-        });
-
-        builder.setNegativeButton("Cancelar", null);
-        builder.create().show();
-    }
-
-    //Codigo para eliminar Actividad
-    private void borrarActividad(Actividad actividad){
-        actividades.remove(actividad);
-        adapter.notifyDataSetChanged();
-        nombres.remove(actividad.getNombre());
-        updateStatus();
-    }
-
-    //Actualiza el numero de actividades
-    private void updateStatus(){
-        TextView tvCantidad = (TextView) findViewById(R.id.tvCantidad);
-        int cantidad = adapter.getCount();
-
-        tvCantidad.setText(Integer.toString(cantidad));
-    }
-
-    //Cuando la app pasa a segundo plano, haremos las comprobaciones de las fechas
-    protected void onResume(){
-        super.onResume();
-
-        //Obtenemos la fecha actual
-        Date fechaActual = new Date();
-
-        //Recorremos la lista de las actividades para comprobar sus fechas
-        for(Actividad actividad: actividades){
-            Date fechaLimite = actividad.getFechaLimite();
-            if(fechaActual.after(fechaLimite)){
-                mostrarAdvertencia(actividad);
-            }
+    public double calcularTotalKm(){
+        double total = 0;
+        for(Entrenamiento entreno: entrenos){
+            total += entreno.getDistancia();
         }
+
+        return total;
     }
 
+    public double calcularMediaMinKm(){
+        double total = 0;
+        for(Entrenamiento entreno: entrenos){
+            total += entreno.getMinutosPorKm(); //Sumamos todos los minutos por km de cada entreno
+        }
 
-    //Mostraremos los cuadros de dialogo para advertir de que actividades han caducado
-    private void mostrarAdvertencia(Actividad actividad){
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("ADVERTENCIA");
-        builder.setMessage("La actividad \"" + actividad.getNombre() + "\" ha caducado");
-        builder.setNegativeButton("Aceptar", null);
-        builder.create().show();
+        return total/adapter.getCount(); //Devolvemos la media diviendo el total entre el numero de entrenamientos
     }
+
 }
